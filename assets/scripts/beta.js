0da4acf7a5f40e217514125ee7f5d84193c25cfb
Brandon Alexander <brandonralexander@gmail.com>
2024-05-27 19:12:53 +0100

    SEO name, centre nav on mobile

--- assets/scripts/beta.js
+++ assets/scripts/beta.js
@@ -2,10 +2,10 @@
  * ONLY SET ACTIVATED TO TRUE WHEN IN USE ON THE BETA VERSION OF THE WEBSITE.
  */
 
-const ACTIVATED = true;
+const ACTIVATED = false;
 const RELEASE_CANDIDATE = true;
 const WEBSITE_VERSION = 2.3;
-const BETA_RELEASE_NUMBER = 1;
+const BETA_RELEASE_NUMBER = 2;
 
 if (ACTIVATED) {
     document.querySelector("body").insertAdjacentHTML("beforeend", `<a href="https://github.com/brandalexa/eloisagodfray.co.uk/releases" target="_blank" id="version" style="text-align: left;">Version: Beta ${WEBSITE_VERSION}${RELEASE_CANDIDATE ? ", Release Candidate Build #" + BETA_RELEASE_NUMBER : ", Build #" + BETA_RELEASE_NUMBER}<br>Pre-release, <u>do not distribute.</u></a>`);
