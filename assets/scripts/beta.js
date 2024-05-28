b5dbdc579586085cbfe3dec4604a3a090e1d92e3
Brandon Alexander <brandonralexander@gmail.com>
2024-05-28 01:25:29 +0100

    Add navbar to home and work

--- assets/scripts/beta.js
+++ assets/scripts/beta.js
@@ -2,11 +2,12 @@
  * ONLY SET ACTIVATED TO TRUE WHEN IN USE ON THE BETA VERSION OF THE WEBSITE.
  */
 
-const ACTIVATED = false;
-const RELEASE_CANDIDATE = true;
-const WEBSITE_VERSION = 2.3;
-const BETA_RELEASE_NUMBER = 2;
+const ACTIVATED = true;
+const RELEASE_CANDIDATE = false;
+const IS_ALPHA = true;
+const WEBSITE_VERSION = "3.0";
+const BETA_RELEASE_NUMBER = 1;
 
 if (ACTIVATED) {
-    document.querySelector("body").insertAdjacentHTML("beforeend", `<a href="https://github.com/brandalexa/eloisagodfray.co.uk/releases" target="_blank" id="version" style="text-align: left;">Version: Beta ${WEBSITE_VERSION}${RELEASE_CANDIDATE ? ", Release Candidate Build #" + BETA_RELEASE_NUMBER : ", Build #" + BETA_RELEASE_NUMBER}<br>Pre-release, <u>do not distribute.</u></a>`);
+    document.querySelector("body").insertAdjacentHTML("beforeend", `<a href="https://github.com/brandalexa/eloisagodfray.co.uk/releases" target="_blank" id="version" style="text-align: left;">Version: ${IS_ALPHA ? "Alpha" : "Beta"} ${WEBSITE_VERSION}${RELEASE_CANDIDATE ? ", Release Candidate Build #" + BETA_RELEASE_NUMBER : ", Build #" + BETA_RELEASE_NUMBER}<br>Pre-release, <u>do not distribute.</u></a>`);
 }
